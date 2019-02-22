3149891c028bdd2da14f773d31f597af9bd56fd1
Administrator <67647091@qq.com>
2019-02-22 20:25:42 +0800

    判断一个字符串是否是回文

--- Text.py
+++ Text.py
@@ -505,11 +505,37 @@
 # print(list)
 
 #四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少
-count=0
-for i in range(1,5):
-    for j in range(1,5):
-        for k in range(1,5):
-            if i!=j and i!=k and j!=k:
-                count+=1
-                print("无重复数字有：",i,j,k)
-print("共有无重复数字%d个"%count)
+# count=0
+# for i in range(1,5):
+#     for j in range(1,5):
+#         for k in range(1,5):
+#             if i!=j and i!=k and j!=k:
+#                 count+=1
+#                 print("无重复数字有：",i,j,k)
+# print("共有无重复数字%d个"%count)
+
+#编写一个函数，判断一个字符串是否为回文，如果是回文返回整数1，否则返回返 回整数0（20分）
+#	回文：就是前后对称。例如：”abba”，”evilolive”是回文，”abxd”和”efga”不是回文
+def a (str):
+    if len(str)%2 == 0:
+        str1 = str[:len(str)//2]
+        str2 = str[len(str)//2:]
+        str1=list(str1)
+        str2 = list(str2)
+        str2.reverse()
+        if str1 == str2:
+            return 1
+        else:
+            return 0
+    elif len(str)%2 != 0:
+        str3 = str[:len(str)//2+1]
+        str4 = str[len(str)//2:]
+        str3 = list(str3)
+        str4 = list(str4)
+        str4.reverse()
+        if str3 == str4:
+            return 1
+        else:
+            return 0
+
+print(a('aberba'))
